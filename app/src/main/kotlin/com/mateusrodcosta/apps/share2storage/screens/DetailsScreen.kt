ffa7e15b2965dfad73dd75644c60087dfc814342
Mateus Rodrigues Costa <mateusrodcosta@gmail.com>
2024-10-11 10:17:33 -0300

    Refactor Details screen layout
    Now using ListItem Composable

--- app/src/main/kotlin/com/mateusrodcosta/apps/share2storage/screens/DetailsScreen.kt
+++ app/src/main/kotlin/com/mateusrodcosta/apps/share2storage/screens/DetailsScreen.kt
@@ -23,13 +23,8 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
@@ -43,6 +38,7 @@ import androidx.compose.material.icons.rounded.Download
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.ListItem
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -146,9 +142,11 @@ fun FileDetailsPortrait(uriData: UriData) {
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.SpaceEvenly
     ) {
-        Box(modifier = Modifier
-            .fillMaxSize()
-            .weight(1.0f)) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .weight(1.0f)
+        ) {
             FilePreview(uriData)
         }
         Box {
@@ -199,22 +197,11 @@ fun FileInfo(uriData: UriData) {
 
 @Composable
 fun FileInfoLine(label: String, content: String) {
-    Box(modifier = Modifier
-        .fillMaxWidth()
-        .clickable { }) {
-        Column(
-            modifier = Modifier
-                .padding(PaddingValues(horizontal = 16.dp, vertical = 8.dp))
-                .heightIn(min = 48.dp), horizontalAlignment = Alignment.Start
-        ) {
-            Text(
-                label,
-                style = MaterialTheme.typography.titleLarge.copy(color = MaterialTheme.colorScheme.tertiary)
-            )
-            Spacer(modifier = Modifier.height(8.dp))
-            Text(content, softWrap = true, style = MaterialTheme.typography.bodyLarge)
-        }
-    }
+    ListItem(modifier = Modifier.clickable { }, headlineContent = {
+        Text(label)
+    }, supportingContent = {
+        Text(content, softWrap = true)
+    })
 }
 
 @Composable
