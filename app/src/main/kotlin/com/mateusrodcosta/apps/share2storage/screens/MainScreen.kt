f48aaa8f2bf3cafc04d2d8188c4e6bcb2b74dc86
Mateus Rodrigues Costa <mateusrodcosta@gmail.com>
2024-10-11 14:01:39 -0300

    Minor import tweak

--- app/src/main/kotlin/com/mateusrodcosta/apps/share2storage/screens/MainScreen.kt
+++ app/src/main/kotlin/com/mateusrodcosta/apps/share2storage/screens/MainScreen.kt
@@ -52,7 +52,6 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
